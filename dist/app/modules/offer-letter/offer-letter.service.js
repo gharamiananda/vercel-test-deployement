30b0fb3cdc50c4cfc7f3201ef963aa0f167667c6
Ananda Gharami <86967865+gharamiananda@users.noreply.github.com>
2026-02-14 23:53:46 +0530

    Refactor import of p-limit to dynamic import syntax

--- dist/app/modules/offer-letter/offer-letter.service.js
+++ dist/app/modules/offer-letter/offer-letter.service.js
@@ -97,7 +97,7 @@ exports.offerLetterService = {
     },
     createBulkOfferLetters(offerLetters, authUser) {
         return __awaiter(this, void 0, void 0, function* () {
-            const pLimit = (yield Promise.resolve().then(() => __importStar(require("p-limit")))).default;
+    const { default: pLimit } = yield import("p-limit");
             const limit = pLimit(1); // Max 1 at a time
             const results = yield Promise.all(offerLetters.map((data) => limit(() => this.processOneOfferLetter(data, authUser))));
             return results;
