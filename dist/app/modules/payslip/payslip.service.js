c7b85c37cedbd42f290e554f1d3f3ed066785455
Ananda Gharami <86967865+gharamiananda@users.noreply.github.com>
2026-02-15 10:34:54 +0530

    Update payslip.service.js

--- dist/app/modules/payslip/payslip.service.js
+++ dist/app/modules/payslip/payslip.service.js
@@ -112,8 +112,10 @@ exports.payslipService = {
     },
     createBulkOfferLetters(offerLetters, authUser) {
         return __awaiter(this, void 0, void 0, function* () {
-            const pLimit = (yield Promise.resolve().then(() => __importStar(require("p-limit")))).default;
-            const limit = pLimit(1); // Max 1 at a time
+           
+               const { default: pLimit } = yield import("p-limit");
+
+    const limit = pLimit(1); // Max 1 at a time
             const results = yield Promise.all(offerLetters.map((data) => limit(() => {
                 if (data === null || data === void 0 ? void 0 : data.employeeEmail) {
                     return processOnePayslipLetter(data, authUser);
